b53dd6235d8e8207ada6799150ae71120c5d3b8d
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-04-03 15:20:52 -0700

    👷🏾‍♀️ constructor added to Square component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() =>
-          alert('click')}>
-          {this.props.value}
-      </button>
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+    render() {
+        return (
+          <button className="square" onClick={() =>
+              alert('click')}>
+              {this.props.value}
+          </button>
     );
   }
 }
